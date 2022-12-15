d01f7bde468262ac4c96eda8cdf1fb09b8a34aba
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-12-15 16:40:44 +0700

    Build Login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Home() {
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios.get("http://localhost:4001/welcome", {
+        headers: {
+          "x-access-token": localStorage.getItem("x-access-token"),
+        },
+      });
+      console.log(result.data);
+      setUser(result.data);
+    }
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Home</h1>
+      <p>{user}</p>
     </div>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,52 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+
+    const result = await axios.post("http://localhost:4001/login", {
+      email,
+      password,
+    });
+    console.log(result.data.token);
+    localStorage.setItem("x-access-token", result.data.token);
+  };
+
   return (
     <div>
       <h1>Login Page</h1>
+      <form>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={onChangeEmail}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" name="password" onChange={onChangePassword} />
+        </div>
+        <div>
+          <button type="submit" onClick={onLogin}>
+            Login
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
